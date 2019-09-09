0822fa7cf5fea3319bee483061a2d3a59e18ee2f
Tyler Bainbridge <tyler@tylerbainbridge.com>
2019-09-09 18:34:32 -0400

    Working deploy with favicon

--- server/src/helpers/files.ts
+++ server/src/helpers/files.ts
@@ -10,7 +10,7 @@ export const getS3Key = (user: User, file: File, type?: KEY_TYPES) =>
   `users/${user.id}/${file.id}/${type || KEY_TYPES.full}.${file.extension}`;
 
 export const getCloudFrontURL = (user: User, file: File, type?: KEY_TYPES) =>
-  `http://collections-file-storage.s3-us-west-2.amazonaws.com/${getS3Key(
+  `https://collections-file-storage.s3-us-west-2.amazonaws.com/${getS3Key(
     user,
     file,
     type,
