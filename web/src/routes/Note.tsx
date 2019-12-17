561dd28a70a4a69b8f33c76d291abaec746777c2
Tyler Bainbridge <tyler@tylerbainbridge.com>
2019-12-16 21:02:03 -0500

    remove yarn lock

--- web/src/routes/Note.tsx
+++ web/src/routes/Note.tsx
@@ -5,6 +5,16 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { MarkdownEditor } from '../components/MarkdownEditor';
 
+const EditorContainer = styled.div`
+  margin: 20px;
+  width: 800px;
+  min-height: 500px;
+  border-radius: 5px;
+  -webkit-box-shadow: 0px 4px 13px 3px rgba(176, 176, 176, 0.93);
+  -moz-box-shadow: 0px 4px 13px 3px rgba(176, 176, 176, 0.93);
+  box-shadow: 0px 4px 13px 3px rgba(176, 176, 176, 0.93);
+`;
+
 const initialVal =
   localStorage.getItem('_editorVal') ||
   `
@@ -35,13 +45,3 @@ export const Note = () => {
     </div>
   );
 };
-
-const EditorContainer = styled.div`
-  margin: 20px;
-  width: 800px;
-  min-height: 500px;
-  border-radius: 5px;
-  -webkit-box-shadow: 0px 4px 13px 3px rgba(176, 176, 176, 0.93);
-  -moz-box-shadow: 0px 4px 13px 3px rgba(176, 176, 176, 0.93);
-  box-shadow: 0px 4px 13px 3px rgba(176, 176, 176, 0.93);
-`;
