2684f43579966fc76f3393bbf62bd2e133b6fdeb
Tyler Bainbridge <tyler@tylerbainbridge.com>
2020-01-07 21:20:06 -0500

    pls work

---
+++ web/build/precache-manifest.a17e703b9a553e10718b768699ca5ca7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9a992f40e1b27cd111c89554482ce5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4895c291fd3c8a98460",
+    "url": "/static/js/2.b4dc45cc.chunk.js"
+  },
+  {
+    "revision": "5c7844fe086f582b7bb7c3d3102c0c43",
+    "url": "/static/js/2.b4dc45cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45be5d9923edc295fcd7",
+    "url": "/static/js/main.4ddec9ff.chunk.js"
+  },
+  {
+    "revision": "daeab959839f0958c22c",
+    "url": "/static/js/runtime-main.0f7d3ee9.js"
+  }
+]);
