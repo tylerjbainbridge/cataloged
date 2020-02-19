c27d797464999b078fdd7473daeee0c7f510d056
Tyler Bainbridge <tyler@tylerbainbridge.com>
2020-02-18 19:55:29 -0500

    progress on mention editor

--- web/src/components/SpotlightSearch.tsx
+++ web/src/components/SpotlightSearch.tsx
@@ -6,7 +6,7 @@ import MentionExample from './MentionEditor';
 
 export const SpotlightSearch = () => {
   return (
-    <Box bg="white" width="100%" rounded="lg" p="10px">
+    <Box bg="white" width="100%" height="50px" rounded="lg" p="10px">
       <MentionExample />
     </Box>
   );
