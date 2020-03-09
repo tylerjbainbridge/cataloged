a4f73cc99cea5e0b78bbe28d2cee1be13e6a5359
Tyler Bainbridge <tyler@tylerbainbridge.com>
2020-03-09 11:35:52 -0400

    better link view

--- web/src/components/LinkDrawer.tsx
+++ web/src/components/LinkDrawer.tsx
@@ -18,15 +18,12 @@ import {
   MenuItem,
   Icon,
   useDisclosure,
-  IconButton,
 } from '@chakra-ui/core';
 import * as yup from 'yup';
 import { useMutation } from '@apollo/client';
-import { useForm } from 'react-hook-form';
-import { useMedia } from 'react-use';
 import Iframe from 'react-iframe';
-
 import { Labels } from './Labels';
+import { useForm } from 'react-hook-form';
 import { LazyImage } from './LazyImage';
 import {
   UPDATE_LINK_MUTATION,
@@ -38,6 +35,7 @@ import { ItemFull_link, ItemFull } from '../graphql/__generated__/ItemFull';
 import { useDebouncedUpdate } from '../hooks/useDebouncedUpdate';
 import { ItemDrawerMeta } from './ItemDrawerMeta';
 import { ItemStatusInput } from './ItemStatusInput';
+import { useMedia } from 'react-use';
 
 export interface ItemWithLink extends ItemFull {
   link: ItemFull_link;
@@ -65,8 +63,7 @@ export const LinkDrawer = ({
   onClose,
   drawerContentProps,
 }: LinkDrawerProps) => {
-  const infoMenuState = useDisclosure(false);
-
+  const editState = useDisclosure(false);
   const isMobile = useMedia('(max-width: 768px)');
 
   const { link } = item;
@@ -109,7 +106,7 @@ export const LinkDrawer = ({
     setValue('href', link.href);
     setValue('title', link.title);
     setValue('description', link.description);
-  }, []);
+  }, [editState.isOpen]);
 
   useDebouncedUpdate(
     updateLink,
@@ -125,7 +122,10 @@ export const LinkDrawer = ({
 
   return (
     <>
-      <DrawerContent width="100vw" bg="black" {...drawerContentProps}>
+      <DrawerContent
+        width={isMobile ? '100%' : editState.isOpen ? '500px' : '700px'}
+        {...drawerContentProps}
+      >
         <Flex
           width="100%"
           float="right"
@@ -133,123 +133,72 @@ export const LinkDrawer = ({
           bg="white"
           position="fixed"
           right="0"
-          // p="20px"
+          p="20px"
           borderLeft="1px solid lightgray"
           justifyContent="space-between"
           flexDirection="column"
           overflowY="scroll"
         >
-          <Box d="flex" maxWidth="100%" height="100%" bg="black" p="0px">
-            {(!isMobile || !infoMenuState.isOpen) && (
-              <Box
-                d="flex"
-                height="100vh"
-                width={isMobile ? '100%' : 'calc(100% - 350px)'}
-                justifyContent="center"
-                alignItems="center"
+          <Box height="100%">
+            <Stack spacing="20px" height="100%">
+              <Flex width="100%" justifyContent="flex-end">
+                <ItemActionMenu item={item}>
+                  {menuNodes => (
+                    <>
+                      <MenuItem d="flex" alignItems="center" onClick={onClose}>
+                        <Icon name="close" fontSize="12px" mr="5px" /> Close
+                      </MenuItem>
+                      <MenuItem
+                        d="flex"
+                        alignItems="center"
+                        onClick={editState.onToggle}
+                      >
+                        <Icon name="edit" fontSize="12px" mr="5px" />{' '}
+                        {editState.isOpen ? 'View link' : 'Edit'}
+                      </MenuItem>
+                      {Object.values(menuNodes)}
+                    </>
+                  )}
+                </ItemActionMenu>
+              </Flex>
+              <Stack
+                spacing={5}
+                display={!editState.isOpen ? 'none' : undefined}
               >
-                <Box position="fixed" top="0px" left="0" p="10px">
-                  <IconButton
-                    icon="arrow-back"
-                    aria-label="arrow-back"
-                    color="white"
-                    variant="ghost"
-                    fontSize="20px"
-                    cursor="pointer"
-                    onClick={onClose}
+                {link.image && (
+                  <LazyImage
+                    rounded
+                    src={link.image}
+                    height="200px"
+                    objectFit="cover"
+                    pt={5}
+                    pb={5}
                   />
-                </Box>
-                <Box position="fixed" top="0px" right="0" p="10px">
-                  <IconButton
-                    icon="info"
-                    aria-label="info"
-                    color="white"
-                    variant="ghost"
-                    fontSize="20px"
-                    cursor="pointer"
-                    onClick={infoMenuState.onOpen}
-                  />
-                </Box>
-                <Box d="flex" justifyContent="center" width="70%" height="100%">
-                  <Iframe
-                    url={link.href}
-                    width="100%"
-                    height="100%"
-                    // id="myId"
-                    // className="myClassname"
-                    // display="initial"
-                    // position="relative"
-                  />
-                </Box>
-              </Box>
-            )}
-            {!isMobile || infoMenuState.isOpen} && (
-            <Box
-              width={isMobile ? '100%' : '350px'}
-              minWidth={isMobile ? '100%' : '350px'}
-              float="right"
-              height="100%"
-              bg="white"
-              position="fixed"
-              right="0"
-              p="20px"
-              justifyContent="space-between"
-              flexDirection="column"
-              overflowY="auto"
-            >
-              <Box>
-                <Stack spacing="20px">
-                  <Flex width="100%" justifyContent="flex-end">
-                    <ItemActionMenu item={item}>
-                      {menuNodes => (
-                        <>
-                          <MenuItem
-                            d="flex"
-                            alignItems="center"
-                            onClick={onClose}
-                          >
-                            <Icon name="close" fontSize="12px" mr="5px" /> Close
-                          </MenuItem>
-                          {Object.values(menuNodes)}
-                        </>
-                      )}
-                    </ItemActionMenu>
-                  </Flex>
-                  <Stack spacing={5}>
-                    {link.image && (
-                      <LazyImage
-                        rounded
-                        src={link.image}
-                        height="200px"
-                        objectFit="cover"
-                        pt={5}
-                        pb={5}
-                      />
-                    )}
-                    <FormControl>
-                      <FormLabel htmlFor="href">URL</FormLabel>
-                      <InputGroup size="md">
-                        <InputLeftAddon
-                          as={Button}
-                          cursor="pointer"
-                          verticalAlign="middle"
-                          // @ts-ignore
-                          variant="outline"
-                          isDisabled={isRefreshing}
-                          isLoading={isRefreshing}
-                          onClick={() => refreshLinkMeta()}
-                          roundedLeft="0"
-                        >
-                          Autofill
-                        </InputLeftAddon>
-                        <Input
-                          name="href"
-                          id="href"
-                          rounded="0"
-                          defaultValue={link.href}
-                          ref={register}
-                        />
-                        {/* <InputRightAddon
+                )}
+                <FormControl>
+                  <FormLabel htmlFor="href">URL</FormLabel>
+                  <InputGroup size="md">
+                    <InputLeftAddon
+                      as={Button}
+                      cursor="pointer"
+                      verticalAlign="middle"
+                      // @ts-ignore
+                      variant="outline"
+                      isDisabled={isRefreshing}
+                      isLoading={isRefreshing}
+                      onClick={() => refreshLinkMeta()}
+                      roundedLeft="0"
+                    >
+                      Autofill
+                    </InputLeftAddon>
+                    <Input
+                      name="href"
+                      id="href"
+                      rounded="0"
+                      defaultValue={link.href}
+                      ref={register}
+                    />
+                    {/* <InputRightAddon
                       as={Button}
                       cursor="pointer"
                       verticalAlign="middle"
@@ -260,61 +209,57 @@ export const LinkDrawer = ({
                     >
                       Go
                     </InputRightAddon> */}
-                      </InputGroup>
-                      <FormErrorMessage>
-                        {errors?.href?.message}
-                      </FormErrorMessage>
-                    </FormControl>
-                    <FormControl>
-                      <FormLabel htmlFor="title">Title</FormLabel>
-                      <Input
-                        name="title"
-                        id="title"
-                        defaultValue={link.title || ''}
-                        ref={register}
-                      />
-                      <FormErrorMessage>
-                        {errors?.title?.message}
-                      </FormErrorMessage>
-                    </FormControl>
-                    <FormControl>
-                      <FormLabel htmlFor="description">Description</FormLabel>
-                      <Textarea
-                        name="description"
-                        id="description"
-                        size="md"
-                        defaultValue={link.description || ''}
-                        ref={register}
-                      />
-                      <FormErrorMessage>
-                        {errors?.description?.message}
-                      </FormErrorMessage>
-                    </FormControl>
-                    <FormControl>
-                      <FormLabel htmlFor="status">Status</FormLabel>
-                      <ItemStatusInput item={item} size="md" />
-                    </FormControl>
-                    <FormControl>
-                      <FormLabel htmlFor="labels">Labels</FormLabel>
-                      <Labels item={item} numDisplayLabels={10} />
-                    </FormControl>
-                    <Divider />
-                    <Button
-                      color="white"
-                      bg="brand.purple.main"
-                      width="100%"
-                      mb="20px"
-                      onClick={() => window.open(link.href, '_blank')}
-                    >
-                      Visit site
-                    </Button>
-                  </Stack>
-                </Stack>
-              </Box>
-              <ItemDrawerMeta item={item} />
-            </Box>
-            )
+                  </InputGroup>
+                  <FormErrorMessage>{errors?.href?.message}</FormErrorMessage>
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="title">Title</FormLabel>
+                  <Input
+                    name="title"
+                    id="title"
+                    defaultValue={link.title || ''}
+                    ref={register}
+                  />
+                  <FormErrorMessage>{errors?.title?.message}</FormErrorMessage>
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="description">Description</FormLabel>
+                  <Textarea
+                    name="description"
+                    id="description"
+                    size="md"
+                    defaultValue={link.description || ''}
+                    ref={register}
+                  />
+                  <FormErrorMessage>
+                    {errors?.description?.message}
+                  </FormErrorMessage>
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="status">Status</FormLabel>
+                  <ItemStatusInput item={item} size="md" />
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="labels">Labels</FormLabel>
+                  <Labels item={item} numDisplayLabels={10} />
+                </FormControl>
+                <Divider />
+                <Button
+                  color="white"
+                  bg="brand.purple.main"
+                  width="100%"
+                  mb="20px"
+                  onClick={() => window.open(link.href, '_blank')}
+                >
+                  Visit site
+                </Button>
+              </Stack>
+              {!editState.isOpen && (
+                <Iframe url={link.href} width="100%" height="100%" id="myId" />
+              )}
+            </Stack>
           </Box>
+          {editState.isOpen && <ItemDrawerMeta item={item} />}
         </Flex>
       </DrawerContent>
     </>
