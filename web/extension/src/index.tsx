7e26b4a0385e6c1653c5c5d87e418ee3ab99e6be
Tyler Bainbridge <tyler@tylerbainbridge.com>
2020-03-17 10:26:32 -0400

    try to deploy on render

--- web/extension/src/index.tsx
+++ web/extension/src/index.tsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
+// import browser from 'webextension-polyfill';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div style={{ width: '100px', height: '300px' }}>
-        Your App injected to DOM correctly!
-      </div>
-    );
-  }
-}
+const App = () => {
+  useEffect(() => {
+    const redirectURL = window.chrome.identity.getRedirectURL();
+    const { oauth2 } = window.chrome.runtime.getManifest();
+    const clientId = oauth2.client_id;
+    const authParams = new URLSearchParams({
+      client_id: clientId,
+      response_type: 'token',
+      redirect_uri: redirectURL,
+      scope: ['email'].join(' '),
+    });
+    const authURL = `https://accounts.google.com/o/oauth2/auth?${authParams.toString()}`;
+    window.chrome.identity
+      .launchWebAuthFlow({ url: authURL, interactive: true })
+      .then(responseUrl => {
+        console.warn({ responseUrl, authURL });
+        const url = new URL(responseUrl);
+        const urlParams = new URLSearchParams(url.hash.slice(1));
+        const params = Object.fromEntries(urlParams.entries()); // access_token, expires_in
+
+        fetch(
+          `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`,
+          {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+          },
+        )
+          .then(response => response.json())
+          .then(data => {
+            alert(JSON.stringify(data));
+          });
+      })
+      .catch(error => {
+        console.warn(error.message, authURL);
+      });
+  });
+
+  return (
+    <div style={{ width: '500px', height: '300px' }}>
+      Your App injected to DOM correctly!
+    </div>
+  );
+};
 
 // // Message Listener function
 // chrome.runtime.onMessage.addListener((request, sender, response) => {
