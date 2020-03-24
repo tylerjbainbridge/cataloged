09498c80df2d7162f2569562ae35fae934de835d
Tyler Bainbridge <tyler@tylerbainbridge.com>
2020-03-24 18:19:31 -0400

    fix landing page

--- apps/server/generate.sh
+++ apps/server/generate.sh
@@ -2,6 +2,9 @@ if [ -f ../../.env ]; then
   source ../../.env;
 fi
 
-echo "Running prisma generate ($POSTGRESQL_URL)";
-
-prisma2 generate
+if [[ -z "${POSTGRESQL_URL}" ]]; then
+  echo "Running prisma generate ($POSTGRESQL_URL)";
+  prisma2 generate
+else
+  echo "Env variable POSTGRESQL_URL not found";
+fi
